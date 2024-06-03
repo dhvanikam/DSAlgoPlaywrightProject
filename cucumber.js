5dfcd48d8c58a72f6779833055c25a0d4020a87f
tohfademo <tohfademo0@gmail.com>
2024-06-03 09:23:01 -0500

    added retry property in cucumber.js file

--- cucumber.js
+++ cucumber.js
@@ -8,7 +8,8 @@ module.exports = {
       'features/step_definitions/*.js'    
     ],
     forceExit: true,
-    format: ["html:cucumber-report.html"]//Allure : [path.resolve(__dirname, "reporter.js")],
+    format: ["html:cucumber-report.html"]/*Allure : [path.resolve(__dirname, "reporter.js")]*/,
+    retry: 1
   },
 };
 
