5a73dddcce48e2d3c7392b240a186fbacd3ac7ab
Dhvanika <dhvanika.patel@gmail.com>
2024-06-05 16:57:39 -0400

    updated cucumber js

--- cucumber.js
+++ cucumber.js
@@ -8,7 +8,8 @@ module.exports = {
       'features/step_definitions/*.spec.js'    
     ],
     forceExit: true,
-    format: [path.resolve(__dirname, "reporter.js")],
+    // format: [path.resolve(__dirname, "reporter.js")],
+    format: ["html:cucumber-report.html"],
     retry: 1,
     //parallel: 3
   },
