7c2270de948353de21eeb75c02fb113456cc4c9e
Dhvanika <dhvanika.patel@gmail.com>
2024-06-07 17:13:49 -0400

    Updated cucumber js for allure report

--- cucumber.js
+++ cucumber.js
@@ -3,12 +3,14 @@ const path = require("path");
 module.exports = {
   default: {
     defaultTimeout: 10000,
-    require: [      
+    require: [
       'features/support/hooks.js',
-      'features/step_definitions/*.spec.js'    
+      'features/step_definitions/*.spec.js'
     ],
     forceExit: true,
-    format:["html:reports/cucumber-report.html", "json:reports/cucumber-report.json"],    
+    //format: [path.resolve(__dirname, "reporter.js")],
+    //format: ["html:cucumber-report.html"],
+    format: ["html:reports/cucumber-report.html", "json:reports/cucumber-report.json",path.resolve(__dirname, 'reporter.js')],
     retry: 1,
     parallel: 3
 
